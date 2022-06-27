388842157fcb762e3a0b8fdbed8114ed0bc9c92d
Ngowda94 <Nagraj94>
2022-06-27 20:10:14 +0530

    increment and decrement

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -1,7 +1,30 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Gambler {
 
-    final static int STAKE = 100;
+    static int STAKE = 100;
     final  static int BET = 1;
+    int betting;
+
+    void uc2(){
+        Random random = new Random();
+        betting = random.nextInt(2);
+        if(betting == 0){
+            STAKE--;
+            System.out.println("you loose 1 points ");
+            System.out.println("your current point is " + STAKE);
+        }
+        else {
+            STAKE++;
+            System.out.println("you won 1 points ");
+            System.out.println("your current point is " + STAKE);
+        }
+    }
+
+    public static void main(String[] args) {
+        Gambler game = new Gambler();
+        game.uc2();
+    }
 }
